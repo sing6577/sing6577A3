bfcaebebfae6f1d46465b3c5fdc6e1546586d815
sing6577 <52467442+sing6577@users.noreply.github.com>
2019-07-23 18:42:52 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,10 @@
+import {BookInfo} from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[] =
+[
+{bname: 'book1', bauthor:'author1',bookgenre:'genre1',byear:'year 1',bpicture:"assets/images/1.jpg"},
+{bname: 'book2', bauthor:'author2',bookgenre:'genre2',byear:'year2',bpicture:"assets/images/2.jpg"},
+{bname: 'book3', bauthor:'author3',bookgenre:'genre3',byear:'year3',bpicture:"assets/images/3.jpeg"},
+{bname: 'book4', bauthor:'author4',bookgenre:'genre4',byear:'year4',bpicture:"assets/images/4.png"}
+
+]
